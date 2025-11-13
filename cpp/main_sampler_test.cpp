f842355e8dc108740cb48e826b4fe6b1b6784cd3
mjruhman <128708542+mjruhman@users.noreply.github.com>
2025-11-13 09:47:25 -0600

    Add files via upload

---
+++ cpp/main_sampler_test.cpp
@@ -0,0 +1,132 @@
+/*****************************************************************//**
+ * @file main_sampler_test.cpp
+ *
+ * @brief Basic test of nexys4 ddr mmio cores
+ *
+ * @author p chu
+ * @version v1.0: initial release
+ *********************************************************************/
+
+// #define _DEBUG
+#include "chu_init.h"
+#include "gpio_cores.h"
+#include "spi_core.h"
+
+/**
+ * blink once per second for 5 times.
+ * provide a sanity check for timer (based on SYS_CLK_FREQ)
+ * @param led_p pointer to led instance
+ */
+void timer_check(GpoCore *led_p) {
+   int i;
+
+   for (i = 0; i < 5; i++) {
+      led_p->write(0xffff);
+      sleep_ms(500);
+      led_p->write(0x0000);
+      sleep_ms(500);
+      debug("timer check - (loop #)/now: ", i, now_ms());
+   }
+}
+
+/**
+ * check individual led
+ * @param led_p pointer to led instance
+ * @param n number of led
+ */
+void led_check(GpoCore *led_p, int n) {
+   int i;
+
+   for (i = 0; i < n; i++) {
+      led_p->write(1, i);
+      sleep_ms(100);
+      led_p->write(0, i);
+      sleep_ms(100);
+   }
+}
+
+
+/**
+ * Test adxl362 accelerometer using SPI
+ */
+
+void gsensor_check(SpiCore *spi_p, GpoCore *led_p) {
+   const uint8_t RD_CMD = 0x0b;
+   const uint8_t PART_ID_REG = 0x02;
+   const uint8_t DATA_REG = 0x08;
+   const float raw_max = 127.0 / 2.0;  //128 max 8-bit reading for +/-2g
+
+   int8_t xraw, yraw, zraw;
+   float x, y, z;
+   int id;
+
+   spi_p->set_freq(400000);
+   spi_p->set_mode(0, 0);
+   // check part id
+   spi_p->assert_ss(0);    // activate
+   spi_p->transfer(RD_CMD);  // for read operation
+   spi_p->transfer(PART_ID_REG);  // part id address
+   id = (int) spi_p->transfer(0x00);
+   spi_p->deassert_ss(0);
+   // read 8-bit x/y/z g values once
+   spi_p->assert_ss(0);    // activate
+   spi_p->transfer(RD_CMD);  // for read operation
+   spi_p->transfer(DATA_REG);  //
+   xraw = spi_p->transfer(0x00);
+   yraw = spi_p->transfer(0x00);
+   zraw = spi_p->transfer(0x00);
+   spi_p->deassert_ss(0);
+   x = (float) xraw / raw_max;
+   y = (float) yraw / raw_max;
+   z = (float) zraw / raw_max;
+
+   // Threshold for 1g reading (raw value is ~63.5 from 127 / 2 above)
+   const int8_t THRESHOLD = 40;
+
+    // Check orientation and light the corresponding LED
+   if (yraw > THRESHOLD) {        // +1g on Y-axis
+      led_p->write(0x08); 
+   } else if (xraw > THRESHOLD) { // +1g on X-axis
+      led_p->write(0x01); 
+   } else if (yraw < -THRESHOLD) { // -1g on Y-axis
+      led_p->write(0x02); 
+   } else if (xraw < -THRESHOLD) { // -1g on X-axis
+      led_p->write(0x04); 
+   } else {
+      led_p->write(0x00); 
+   }
+}
+
+
+
+
+
+/**
+ * core test
+ * @param led_p pointer to led instance
+ * @param sw_p pointer to switch instance
+ */
+void show_test_id(int n, GpoCore *led_p) {
+   int i, ptn;
+
+   ptn = n; //1 << n;
+   for (i = 0; i < 20; i++) {
+      led_p->write(ptn);
+      sleep_ms(30);
+      led_p->write(0);
+      sleep_ms(30);
+   }
+}
+
+GpoCore led(get_slot_addr(BRIDGE_BASE, S2_LED));
+SpiCore spi(get_slot_addr(BRIDGE_BASE, S9_SPI));
+
+
+
+int main() {
+
+   while (1) {
+      gsensor_check(&spi, &led);
+      sleep_ms(50);
+   } //while
+} //main
